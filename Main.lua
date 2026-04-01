fc207547dd2dcbe5f5a67e82089d332be2ec01e2
Tekk1no <akmanov21212121@gmail.com>
2026-04-01 19:27:31 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,41 +1,35 @@
--- VANDOLY X CORE ENGINE
-local assetId = 124078092058387
+-- VANDOLY X CORE ENGINE (WORKING VERSION)
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local assetId = 124078092058387 -- Твой ассет
 
+-- Загружаем модель
 local success, model = pcall(function()
     return game:GetObjects("rbxassetid://" .. assetId)[1]
 end)
 
-if success and model then
-    -- Чистим старое
-    if game.CoreGui:FindFirstChild("VandolyX") then
-        game.CoreGui.VandolyX:Destroy()
-    end
-
-    model.Name = "VandolyX"
-    model.Parent = game:GetService("CoreGui")
-    
-    local mainFrame = model:FindFirstChild("ZenithMainFrame")
-    local mainScript = mainFrame:FindFirstChild("MainScript") or model:FindFirstChild("MainScript", true)
-
-    if mainScript and mainScript:IsA("LuaSourceContainer") then
-        -- СОЗДАЕМ ОКРУЖЕНИЕ (Чтобы твой script.Parent сработал)
-        local env = setmetatable({
-            script = mainScript
-        }, {
-            __index = getfenv()
-        })
-        
-        local func, err = loadstring(mainScript.Source)
-        if func then
-            setfenv(func, env)
-            task.spawn(func)
-            print("Vandoly X: Твой оригинальный Core Script запущен! 😈")
-        else
-            warn("Ошибка в синтаксисе твоего скрипта: " .. tostring(err))
-        end
-    else
-        warn("Vandoly X: MainScript не найден в модели!")
-    end
-else
-    warn("Vandoly X: Ошибка загрузки ассета!")
+if not (success and model) then
+    warn("Vandoly X: Не удалось загрузить ассет!")
+    return
 end
+
+-- Удаляем старое, если есть
+if player.PlayerGui:FindFirstChild("VandolyX") then
+    player.PlayerGui.VandolyX:Destroy()
+end
+
+model.Name = "VandolyX"
+model.Parent = player.PlayerGui -- вот ключ: PlayerGui
+
+-- Ищем MainScript внутри модели
+local mainFrame = model:FindFirstChild("ZenithMainFrame") or model:FindFirstChildWhichIsA("Frame", true)
+local mainScript = mainFrame and mainFrame:FindFirstChild("MainScript") or model:FindFirstChild("MainScript", true)
+
+if not (mainScript and mainScript:IsA("LocalScript")) then
+    warn("Vandoly X: MainScript не найден или не LocalScript!")
+    return
+end
+
+-- Всё остальное просто работает, никаких костылей с loadstring
+mainScript.Disabled = false -- включаем скрипт
+print("Vandoly X: MainScript запущен и функционал активен! 😈")
